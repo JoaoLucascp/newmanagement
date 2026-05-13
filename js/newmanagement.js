7f4289144cf5d28374f7117dcaaefbacfd742b5f
Sr.Joao <joaojlk.naipe@gmail.com>
2026-05-13 16:17:34 -0300

    fix: corrige paths de CSS e JS para public/ (GLPI 11)

--- js/newmanagement.js
+++
@@ -1,8 +0,0 @@
-/**
- * Newmanagement Plugin - Scripts JavaScript
- */
-
-/* Adicione aqui os scripts do plugin conforme necessário */
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Newmanagement Plugin carregado.');
-});
